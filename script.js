9e1619c6549daa5f641f9b5c5ce311c9ca6ec7bb
lavarellocambaceresmarcos <lavarellomarcos@gmail.com>
2021-01-20 15:32:56 -0300

    entrega del desafío nro-5

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var edad = parseInt(prompt("ingrese su edad"))
+/*var edad = parseInt(prompt("ingrese su edad"))
 
 function permisoDeConsumo (edad) {
 	
@@ -15,11 +15,6 @@ function permisoDeConsumo (edad) {
 }
 
 permisoDeConsumo (edad) 
-
-
-
-
-
 
 
 
@@ -70,6 +65,146 @@ function resumenNota () {
 for (var i = 0; i < cantidadAlumnos; i++) {
   resumenNota()
 }*/
+
+
+/*var persona = new object()
+
+persona.nombre = "Marcos"  /*también se puede escribir de esta manera---- persona["nombre"]="Marcos"*/
+
+/*persona.edad= 24
+
+persona.altura= 1.80
+
+persona.catlover= false
+
+persona.doglover= true */
+
+
+/*var Marcos= new Persona()*/
+
+/*function Persona (nombre,edad,altura,catlover,doglover) {
+	this.nombre= nombre
+	this.edad=edad
+	this.altura=altura
+}
+
+var nombre= prompt("ingrese su nombre")
+
+var edad = prompt ("ingrese su edad")
+
+var altura = prompt ("ingrese su altura")
+
+var persona= new Persona (nombre,edad,altura)
+console.log (persona)
+
+
+var propiedad = prompt("qué dato querés saber sobre la persona")
+	if (!Persona[propiedad]) {
+		console.log("no contamos con ese dato")
+	} else console.log(Persona[propiedad])
+
+
+console.log(Persona[propiedad])*/
+
+
+
+/*Esto es un objeto*/
+
+/*var miAuto = new object()
+
+miAuto.modelo ="ford-mustang"
+
+miAuto.marca= "ford"
+
+miAuto.anio= 1970
+
+miAuto.color= "gris"
+
+miAuto.patente="edg 123"*/
+
+
+/*Esto es un constructor, en este caso, las propiedades están anidadas-dentro de una función, que luego son utilizadas por una variable*/
+
+
+/*function cliente (edad, peso, nacimiento,sexo) {
+	this.edad=edad
+	this.peso=peso
+	this.nacimiento=nacimiento
+	this.sexo=sexo
+
+	this.ingresar= function () {return "El cliente ingresó a la plataforma"};
+
+	this.getEdad= function () {return this.edad};
+
+	this.getPeso=function() {return this.peso};
+
+	this.getNacimiento =function() {return this.nacimiento};
+
+	this.getSexo=function() {return this.sexo};
+
+	
+}
+
+var cliente1 = new cliente("25","82","1996","Masculino");
+
+var cliente2 = new cliente("23", "65", "1994", "Femenino");
+
+console.log (cliente1.ingresar ())
+
+console.log (cliente1.getEdad())
+
+console.log (cliente1.getSexo())
+
+console.log (cliente2.ingresar())
+
+console.log (cliente2.getEdad())
+
+console.log (cliente2.getPeso())*/
+
+
+
+
+/* DESAFÍO DE LA CLASE 5---- OBJETOS*/
+
+function permisoDeConsumo (edad) {
+	
+
+	if (edad<=18) {
+		console.log ("usted es menor de edad, no puede consumir alcohol")
+		alert ("usted es menor de edad,no puede consumir alcohol")
+		var edad = parseInt(prompt("ingrese su edad"))
+		permisoDeConsumo (edad)
+	} else {
+		console.log ("Bienvenido, disfrute su compra") 
+		alert ("Bienvenido,disfrute su compra")
+	}
+}
+
+
+
+function cliente (nombre,edad) {
+	this.nombre=nombre
+	this.edad=edad
+}
+var nombre= prompt("ingrese su nombre")
+
+var edad = prompt ("ingrese su edad")
+	permisoDeConsumo (edad)
+
+var cliente= new cliente (nombre,edad)
+console.log (cliente)
+
+
+var datosCliente = prompt("Quisiera  usted recuperar algún dato(nombre o edad?")
+	if( (!cliente[datosCliente]) ) {
+		alert("no contamos con ese dato")
+		console.log("no contamos con ese dato")
+	} else { 
+		alert(cliente[datosCliente])
+	} if (datosCliente=="no") {
+		alert ("Continúe a la página")
+	}
+
 
 
 
